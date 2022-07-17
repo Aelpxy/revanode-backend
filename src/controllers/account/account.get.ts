8642c757155e40e44fab53300b4830a573af697d
Aelpxy <84912564+Aelpxy@users.noreply.github.com>
2022-07-17 17:16:27 +0000

    feat: add auth.ts middleware

---
+++ src/controllers/account/account.get.ts
@@ -0,0 +1,10 @@
+import { Response, Request } from "express";
+import { PrismaClient } from "@prisma/client";
+
+export const accountInformation = async (req: Request, res: Response) => {
+    try {
+        
+    } catch (error) {
+        
+    };
+};
---
+++ src/middlewares/auth.ts
@@ -0,0 +1,34 @@
+import jwt from "jsonwebtoken";
+import { Response, Request, NextFunction } from "express";
+
+export const authorizeUser = async (req: Request, res: Response, next: NextFunction) => {
+    let token: any;
+
+    if (req.headers.authorization && req.headers.authorization?.startsWith('Bearer')) {
+        try {
+
+            token = req.headers.authorization?.split(' ')[1];
+
+            // @ts-ignore
+            const decode = jwt.verify(token, process.env.JWT_SECRET_TOKEN);
+            // @ts-ignore
+            req.user = decode;
+
+            next();
+            
+        } catch (error) {
+            res.status(401).send({
+                message: "UNAUTHORIZED",
+                payload: null,
+            });
+        };
+    };
+    if (!token) {
+        res.status(401).send({
+            message: "NO_AUTH_TOKEN_INCLUDED",
+            payload: null,
+        });
+
+        return;
+    };
+};
