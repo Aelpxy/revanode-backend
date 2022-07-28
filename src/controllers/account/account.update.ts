70fe1bab912f4e2e5d2be9f226e21f7dbefddeed
Aelpxy <84912564+Aelpxy@users.noreply.github.com>
2022-07-28 13:19:47 +0000

    add update route

--- src/controllers/account/account.update.ts
+++ src/controllers/account/account.update.ts
@@ -1,7 +1,71 @@
 import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
 
-export const accountUpdate = async (req: Request, res: Response) => {
+const db = new PrismaClient();
+
+interface UserRequest extends Request {
+  user: {
+    apiKey: string;
+  };
+}
+
+export const accountUpdate = async (req: UserRequest, res: Response) => {
   try {
+    if (req.method != "PUT") {
+      res.status(405).send({
+        message: "METHOD_NOT_ALLOWED",
+        payload: null,
+      });
+
+      return;
+    }
+
+    const token = req.user.apiKey;
+
+    if (req.body.password) {
+      const updatedUser = await db.user.update({
+        where: {
+          apiKey: token || undefined,
+        },
+        data: {
+          username: req.body.username || undefined,
+          email: req.body.email || undefined,
+          password: await bcrypt.hash(req.body.password, 15),
+        },
+      });
+
+      res.status(200).send({
+        message: "SUCCESS",
+        payload: updatedUser,
+      });
+      return;
+    }
+
+    if (!req.body.password) {
+      const findUser = await db.user.findUnique({
+        where: {
+          apiKey: token,
+        },
+      });
+
+      const updatedUser = await db.user.update({
+        where: {
+          apiKey: token || undefined,
+        },
+        data: {
+          username: req.body.username || undefined,
+          email: req.body.email || undefined,
+          password: findUser?.password,
+        },
+      });
+      res.status(200).send({
+        message: "SUCCESS",
+        payload: updatedUser,
+      });
+
+      return;
+    }
   } catch (error) {
     res.status(500).send({
       message: "INTERNAL_SERVER_ERROR",
--- src/routes/router.ts
+++ src/routes/router.ts
@@ -8,6 +8,7 @@ import { authLogin } from "../controllers/auth/auth.login";
 
 import { accountInformation } from "../controllers/account/account.get";
 import { accountDelete } from "../controllers/account/account.delete";
+import { accountUpdate } from "../controllers/account/account.update";
 
 import { getServer } from "../controllers/servers/server.get";
 
@@ -36,6 +37,7 @@ router.post("/account/login", authLogin);
 // /account route
 router.get("/account", authorizeUser, accountInformation);
 router.delete("/account/delete", authorizeUser, accountDelete)
+router.put("/account/update", authorizeUser, accountUpdate)
 
 // /servers route
 router.get("/servers", authorizeUser, getServer);
