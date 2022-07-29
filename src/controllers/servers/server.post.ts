516051b56e4ee24d5da26173e011a445b295ecb1
Aelpxy <84912564+Aelpxy@users.noreply.github.com>
2022-07-29 11:35:17 +0000

    sorta made it

--- src/controllers/servers/server.post.ts
+++ src/controllers/servers/server.post.ts
@@ -24,21 +24,25 @@ export const postServer = async (req: Request, res: Response) => {
     if (!user) {
       res.status(404).send({
         message: "NOT_FOUND",
-        payload: null,
+        payload: "User does not exist",
       });
+
+      return;
     }
 
     const server = await db.server.create({
       data: {
         name: req.body.name,
-        panelId: req.body.paneId,
         amount: req.body.amount,
         region: req.body.region,
         paymentId: req.body.paymentId,
-        active: false,
         userId: Number(user?.id),
       },
     });
+
+    res.status(201).send(server)
+
+
   } catch (error) {
     res.status(500).send({
       message: "INTERNAL_SERVER_ERROR",
